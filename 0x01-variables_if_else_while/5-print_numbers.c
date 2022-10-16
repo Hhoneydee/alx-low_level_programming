4c356cfdd2879af24ffa9a9d35491f7897056068
Hhoneydee <dorcasrasheed62@gmail.com>
2022-10-16 16:14:36 -0700

    My commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int var = 0;
+while (var < 10)
+{
+printf("%d", var);
+var++;
+}
+printf("\n");
+return (0);
+}
