935dfa880af1b5ad38172d7802922bee47b11c69
Hhoneydee <dorcasrasheed62@gmail.com>
2022-10-19 17:15:28 -0700

    My commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,16 +1,18 @@
-#ifndef main_h
-#define main_h
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
 
-extern void print_alphabet(void);
-extern void print_alphabet_x10(void);
-extern int _islower(int c);
-extern int _isalpha(int c);
-extern int print_sign(int n);
-extern int _abs(int);
-extern int print_last_digit(int);
-extern void jack_bauer(void);
-extern void times_table(void);
-extern int add(int, int);
-extern void print_to_98(int n);
-extern void print_times_table(int n);
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+void num(int r, int n);
 #endif
